e0c0f4cfd5cf97459ee0e7a9dc9946f1317ff397
unshade <72690981+unshade@users.noreply.github.com>
2022-03-14 11:01:05 +0100

    display images

--- gallery_ui.js
+++ gallery_ui.js
@@ -2,14 +2,14 @@ import conf from "./conf.js";
 
 function display_galerie(gallery) {
     let gallery_container = document.getElementById('gallery_container')
-    gallery_container.innerHTML = gallery.photos.reduce( (previousValue, currentValue) => (
+    gallery_container.innerHTML = gallery.photos.reduce( (previousValue, currentValue) =>
+        previousValue +
         `
         <div class="vignette" >
          <img data-uri="${conf.webetu + currentValue.links.self.href}"
            src="${conf.webetu + currentValue.photo.thumbnail.href}">
        </div>
-        `
-        )
+        `, ""
 
     )
 }

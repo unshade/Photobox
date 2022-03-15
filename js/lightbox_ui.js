6ccbf5981df14d54b0f7ce0626245cd14953e41b
unshade <72690981+unshade@users.noreply.github.com>
2022-03-15 22:55:38 +0100

    Always toggle

--- js/lightbox_ui.js
+++ js/lightbox_ui.js
@@ -6,12 +6,6 @@ function display_lightbox(data) {
 
     title.innerHTML = data.photo.titre
     lightbox_full_img.src = conf.webetu + data.photo.url.href
-
-    let lightbox_close = document.querySelector('#lightbox_close')
-    let lightbox_container = document.getElementById('lightbox_container')
-    lightbox_close.addEventListener("click", () => {
-        lightbox_container.classList.toggle('lightbox_container--hidden')
-    })
 }
 
 export default {
--- js/photobox.js
+++ js/photobox.js
@@ -1,7 +1,11 @@
 import gallery from "./gallery.js";
 import gallery_ui from "./gallery_ui.js";
 
-
+let lightbox_close = document.querySelector('#lightbox_close')
+let lightbox_container = document.getElementById('lightbox_container')
+lightbox_close.addEventListener("click", () => {
+    lightbox_container.classList.toggle('lightbox_container--hidden')
+})
 document.querySelector('#load_gallery')
     .addEventListener('click', e => {
         gallery.load().then(data => gallery_ui.display_galerie(data))

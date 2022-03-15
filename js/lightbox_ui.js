12d0499a3942548b8c8d33cde8b2c1c60911aac8
unshade <72690981+unshade@users.noreply.github.com>
2022-03-15 22:42:50 +0100

    lightbox + photo

--- js/lightbox_ui.js
+++ js/lightbox_ui.js
@@ -5,7 +5,7 @@ function display_lightbox(data) {
     let lightbox_full_img = document.getElementById("lightbox_full_img")
 
     title.innerHTML = data.photo.titre
-    lightbox_full_img.src = conf.webetu + data.photo.href
+    lightbox_full_img.src = conf.webetu + data.photo.url.href
 }
 
 export default {

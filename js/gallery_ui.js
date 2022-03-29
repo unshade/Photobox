054f7f30b4bb6a159872daec221b05fd7546131d
unshade <72690981+unshade@users.noreply.github.com>
2022-03-29 18:21:30 +0200

    img display

--- js/gallery_ui.js
+++ js/gallery_ui.js
@@ -7,15 +7,23 @@ function display_galerie(gallery) {
     gallery_container.innerHTML = gallery.photos.reduce((previousValue, currentValue) =>
         previousValue +
         `
-        <div class="vignette" >
-         <img data-uri="${conf.webetu + currentValue.links.self.href}"
-           src="${conf.webetu + currentValue.photo.thumbnail.href}">
-        </div>
+
+            <li class="relative">
+                <div class="group block w-full aspect-w-10 aspect-h-7 rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden">
+                    <img data-uri="${conf.webetu + currentValue.links.self.href}"
+           src="${conf.webetu + currentValue.photo.thumbnail.href}" alt="" class="object-cover pointer-events-none group-hover:opacity-75">
+                    <button type="button" class="absolute inset-0 focus:outline-none">
+                        <span class="sr-only">View details for IMG_4985.HEIC</span>
+                    </button>
+                </div>
+                <p class="mt-2 block text-sm font-medium text-gray-900 truncate pointer-events-none">IMG_4985.HEIC</p>
+                <p class="block text-sm font-medium text-gray-500 pointer-events-none">3.9 MB</p>
+            </li>
         `, ""
     )
 
     gallery_container.addEventListener('click', async ev => {
-        let data = await lightbox.load(ev.target)
+        let data = await lightbox.load(ev.target.offsetParent.firstElementChild)
         lightbox.updateCurrent(data.photo.id)
         lightbox_ui.display_lightbox(data)
         lightbox_ui.show()

556b2b015abd277d5082bdb04c51fdb74a7a5efa
unshade <72690981+unshade@users.noreply.github.com>
2022-03-29 16:45:47 +0200

    display comments

--- js/lightbox_ui.js
+++ js/lightbox_ui.js
@@ -1,6 +1,7 @@
 import conf from "./conf.js";
+import photoloader from "./photoloader.js";
 
-function display_lightbox(data) {
+async function display_lightbox(data) {
     let title = document.getElementById("lightbox_title")
     let descr = document.getElementById("lightbox_descr")
     let lightbox_full_img = document.getElementById("lightbox_full_img")
@@ -19,6 +20,22 @@ function display_lightbox(data) {
     size.innerHTML = data.photo.size
     imgwidth.innerHTML = data.photo.width
     imgheight.innerHTML = data.photo.height
+
+    let comments = await photoloader.loadRessource(conf.webetu + data.links.comments.href)
+
+    comments = comments.comments
+
+    document.getElementById("comments_container").innerHTML = comments.reduce((acc, cur) =>
+        acc +
+            `
+            <div id="${cur.id}">
+                 <h2>${cur.titre}</h2>
+                 <div>${cur.content}</div>
+                 <div>${cur.pseudo}</div>
+                 <div>${cur.date}</div>
+            </div>
+            `
+    , "")
 
 }
 

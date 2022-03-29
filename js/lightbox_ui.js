248b5d476e6f48cad362fe36cd7254e8512aad6a
unshade <72690981+unshade@users.noreply.github.com>
2022-03-29 13:54:47 +0200

    fin p1

--- js/lightbox_ui.js
+++ js/lightbox_ui.js
@@ -2,10 +2,24 @@ import conf from "./conf.js";
 
 function display_lightbox(data) {
     let title = document.getElementById("lightbox_title")
+    let descr = document.getElementById("lightbox_descr")
     let lightbox_full_img = document.getElementById("lightbox_full_img")
+    let format = document.getElementById("format")
+    let type = document.getElementById("type")
+    let size = document.getElementById("size")
+    let imgwidth = document.getElementById("imgwidth")
+    let imgheight = document.getElementById("imgheight")
+
 
     title.innerHTML = data.photo.titre
     lightbox_full_img.src = conf.webetu + data.photo.url.href
+    descr.innerHTML = data.photo.descr
+    format.innerHTML = data.photo.format
+    type.innerHTML = data.photo.type
+    size.innerHTML = data.photo.size
+    imgwidth.innerHTML = data.photo.width
+    imgheight.innerHTML = data.photo.height
+
 }
 
 function show() {
